efe8acaf4ed4316a51b955adee913c745005347a
renskelotte48 <49675916+renskelotte48@users.noreply.github.com>
2021-06-05 12:30:31 +0200

    Add endpoints for all drug name/concept_id and highest order_id + tests. All tests pass

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 const testPatientEndpoint = require('./test/patient').testPatientEndpoint;
 const testPrescriptionEndpoint = require('./test/prescription').testPrescriptionEndpoint;
 const testMeasurementEndpoint = require('./test/measurement').testMeasurementEndpoint;
+const testDrugsEndpoint = require('./test/drugs').testDrugsEndpoint;
 
 async function testAll() {
+    await testDrugsEndpoint();
     await testMeasurementEndpoint();
     await testPatientEndpoint();
     await testPrescriptionEndpoint();
