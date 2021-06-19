2a981f76b2f1532d9bdb9815558ad1c640eda21a
renskelotte48 <49675916+renskelotte48@users.noreply.github.com>
2021-06-19 17:01:31 +0200

    updated tests, removed greeter call in server

--- test/measurement.js
+++ test/measurement.js
@@ -24,6 +24,17 @@ async function testMeasurementEndpoint() {
     })
 
     await sleep(500);
+
+    request.get('http://localhost:3000/measurements/measurement/13/latest/729', function(err, response, body) {
+        if (err) throw err;
+        if (response.statusCode == 200) {
+            console.log('Get latest platelets measurement for patient 13: OK');
+        } else {
+            console.error('ERROR. Get latest platelet measurement for patient 13 failed with statuscode ' + response.statusCode);
+        }
+    })
+
+    await sleep(500);
 }
 
 module.exports = { testMeasurementEndpoint };
